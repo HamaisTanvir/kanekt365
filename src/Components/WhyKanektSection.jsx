31eaaf8304896c5274152497a4d4dc0e96dd661c
HamaisTanvir <hamais.tanvir@codingkey.com>
2024-07-23 16:44:59 +0500

    button

--- src/Components/WhyKanektSection.jsx
+++ src/Components/WhyKanektSection.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import video2 from "../assets/video2.mp4";
-import {Button} from '@mui/material'
 
 const WhyKanektSection = () => {
   return (
@@ -18,7 +17,6 @@ const WhyKanektSection = () => {
             <h1 className="text-xl sm:text-6xl  font-bold" style={{color: '#222D35', fontSize: '33px'}}>
             WHY KANEKT 365’S CALL CENTER?
             </h1>
-            <Button variant= 'contained' className='max-w-40 h-12'>MUI Button</Button>
              <div className='text-center mt-10'>
               <a href="https://hamaistanvir.com" style={{backgroundColor:'#0773B3'}} className='py-4 px-3 border-none rounded-md text-white text-sm font-semibold'>POS INTEGRATION</a>
              </div>

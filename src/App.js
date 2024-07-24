d559b0b05198b47c54e35696d4fb173ba0fed888
HamaisTanvir <hamais.tanvir@codingkey.com>
2024-07-25 00:15:37 +0500

    Bitrix SignUp Form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
 import HeroSection from './Components/HeroSection';
-import Brands from './Components/Brands';
+// import Brands from './Components/Brands';
 import TopBar from './Components/TopBar';
-import WhyKanektSection from './Components/WhyKanektSection'
+// import WhyKanektSection from './Components/WhyKanektSection'
+import BitrixSignupForm from './Components/BitrixSignupForm';
 
 const App = () => {
   return (
     <div>
       <TopBar />
-        <Navbar />
+      <Navbar />
       <div className='max-w-7xl mx-auto pt-20 px-6'>
         <HeroSection />
       </div>
-        <Brands />
-        <WhyKanektSection />
+      {/* <Brands /> */}
+      {/* <WhyKanektSection /> */}
+      <div className="mt-10">
+        <BitrixSignupForm />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
